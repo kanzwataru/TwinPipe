eeeee0c4ee41d64a9367431f6add7aa501e26422
Wataru <kanzwataru@gmail.com>
2019-02-20 21:51:27 -0500

    - ui done

--- scripts/TwinPipe/core.py
+++ scripts/TwinPipe/core.py
@@ -4,5 +4,5 @@ reload(frontend)
 def launch():
 	parent = ui_lib.get_maya_window()
 
-	frontend_window = frontend.FrontendWindow()
+	frontend_window = frontend.FrontendWindow(parent)
 	ui_lib.show_window('FrontendWindow', frontend_window)
--- scripts/TwinPipe/ui/widgets.py
+++ scripts/TwinPipe/ui/widgets.py
@@ -13,19 +13,25 @@ class TableWidget(QtWidgets.QWidget):
         super(TableWidget, self).__init__(parent)
 
         layout = QtWidgets.QHBoxLayout()
-        button_bar_layout = QtWidgets.QVBoxLayout()
+        self.button_bar_layout = QtWidgets.QVBoxLayout()
         button_bar_widget = QtWidgets.QWidget()
-        button_bar_widget.setLayout(button_bar_layout)
+        button_bar_widget.setLayout(self.button_bar_layout)
 
-        self.table = QtWidgets.QTableView()
+        self.vertical_layout = QtWidgets.QVBoxLayout()
+        vertical_widget = QtWidgets.QWidget()
+        vertical_widget.setLayout(self.vertical_layout)
+
+        self.table = QtWidgets.QTableWidget()
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.vertical_layout.addWidget(self.table)
+
         self.add_button = button('+')
         self.remove_button = button('-')
+        self.button_bar_layout.addWidget(self.add_button)
+        self.button_bar_layout.addWidget(self.remove_button)
+        self.button_bar_layout.addStretch()
 
-        button_bar_layout.addWidget(self.add_button)
-        button_bar_layout.addWidget(self.remove_button)
-        button_bar_layout.addStretch()
-
-        layout.addWidget(self.table)
+        layout.addWidget(vertical_widget)
         layout.addWidget(button_bar_widget)
         self.setLayout(layout)
 
@@ -34,29 +40,29 @@ class AssetWidget(TableWidget):
     def __init__(self, parent=None):
         super(AssetWidget, self).__init__(parent)
 
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(['Name', 'Status', 'Latest Log'])
 
-class EntityWidget(QtWidgets.QWidget):
+
+class EntityWidget(TableWidget):
     def __init__(self, parent=None):
         super(EntityWidget, self).__init__(parent)
 
-        layout = QtWidgets.QVBoxLayout()
         bottom_buttons_layout = QtWidgets.QHBoxLayout()
         bottom_buttons_widget = QtWidgets.QWidget()
         bottom_buttons_widget.setLayout(bottom_buttons_layout)
 
         self.open_button = button('Open')
         self.ref_button = button('Reference')
-        self.show_button = button('Show')
+        self.show_button = button('Show...')
         self.versions_button = button('Versions...')
 
         bottom_buttons_layout.addWidget(self.open_button)
         bottom_buttons_layout.addWidget(self.ref_button)
-        bottom_buttons_layout.addWidget(self.show_button)
         bottom_buttons_layout.addWidget(self.versions_button)
+        bottom_buttons_layout.addWidget(self.show_button)
 
-        self.view = TableWidget()
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(['Pipeline Step', 'Artist', 'Latest Log'])
 
-        layout.addWidget(self.view)
-        layout.addWidget(bottom_buttons_widget)
-
-        self.setLayout(layout)
+        self.vertical_layout.addWidget(bottom_buttons_widget)
