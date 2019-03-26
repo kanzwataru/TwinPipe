b987a7a29d567356a154caee52a2321d51677e78
神崎航 <kanzwataru@gmail.com>
2019-03-26 10:14:02 -0400

    - add cloning - use local tools folder (so people can install at home) - add icons and labels to shelf

--- scripts/TwinPipe/setup/env_setup.py
+++ scripts/TwinPipe/setup/env_setup.py
@@ -1,3 +1,4 @@
+import maya.cmds as cmds
 import pymel.core as pm
 import os.path
 import subprocess
@@ -12,6 +13,12 @@ MBOXX_MAPPED_LETTER = "m:"
 MBOXX_USER = "wataru"
 MBOXX_PASS = "qwaszx"
 
+REPO_PATH = os.path.join(MBOXX_MAPPED_LETTER, '__repository__', 'TwinReaperRepo.git')
+
+def bin_dir():
+    path = cmds.getModulePath(mn='TwinPipeBootstrap')
+    return os.path.normpath(os.path.join(path, 'bin'))
+
 def mboxx_setup():
     try:
         subprocess.call("net use {} /delete".format(MBOXX_MAPPED_LETTER))
@@ -25,16 +32,18 @@ def mboxx_setup():
         MBOXX_USER))
 
 def git_setup():
-    #mboxx_setup()
-    
-    subprocess.call("start {}".format(os.path.join(MBOXX_MAPPED_LETTER, 'tools', 'windows', 'install.bat')))
+    bat = os.path.join(bin_dir(), 'windows_tools', 'install.bat')
+    subprocess.call("start {}".format(bat), shell=True)
 
 def clone_project():
     mboxx_setup()
     
+    path = cmds.fileDialog2(dialogStyle=2, fileMode=3, okCaption='Clone Here')[0]
+    if os.listdir(path):
+        cmds.warning("Please choose an empty folder to copy into")
+        return
     
-    
-    #subprocess.call("cmd /k 
+    subprocess.call("start cmd /k git clone {} {}".format(REPO_PATH, path), shell=True)
 
 def setup_window():
     win_name = "twinPipeSetupUI"
